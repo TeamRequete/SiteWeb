31b79e159979231eab68a7a8604315ffed596389
requin <requin_citron@protonmail.com>
2022-03-08 13:44:33 +0100

    ajout prof bdd

--- docker_conf/database.sql
+++ docker_conf/database.sql
@@ -28,11 +28,15 @@ DROP TABLE IF EXISTS formations_user;
 CREATE OR REPLACE TABLE formations_user(
   user_id int not null,
   formation_id int not null,
-  primary key (user_id, formation_id),
+  prof_id int not null,
+  primary key (user_id, formation_id,prof_id),
   constraint fk_user
     foreign key (user_id) references users(ID) ON DELETE CASCADE,
   constraint fk_formation
-    foreign key (formation_id) references formations(formation_id) ON DELETE CASCADE
+    foreign key (formation_id) references formations(formation_id) ON DELETE CASCADE,
+  constraint fk_prof
+    foreign key (prof_id) references users(ID) ON DELETE CASCADE
+  constraint ct_prof_eleve CHECK(user_id <> prof_id )
 );
 
 SET FOREIGN_KEY_CHECKS=1;
