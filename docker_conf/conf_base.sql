874a7533f8f7a4753f522de6c38a0ee96f902482
requin <requin_citron@protonmail.com>
2022-03-26 12:16:59 +0100

    ajout docker compose avec la meme config que pour l'ancien système

--- docker_conf/conf_base.sql
+++
@@ -1,7 +0,0 @@
-USE mysql;
-FLUSH PRIVILEGES ;
-GRANT ALL ON *.* TO 'root'@'%' identified by 'roottoor' WITH GRANT OPTION ;
-GRANT ALL ON *.* TO 'root'@'localhost' identified by 'roottoor' WITH GRANT OPTION ;
-SET PASSWORD FOR 'root'@'localhost'=PASSWORD('placeholder') ;
-DROP DATABASE IF EXISTS test ;
-FLUSH PRIVILEGES ;
