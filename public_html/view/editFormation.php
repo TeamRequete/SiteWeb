bfcc00d4b567a505ec35af4885ae2975559132ec
LancelotDucoeur <lancelot.ducoeur@gmail.com>
2022-03-29 09:55:12 +0200

    css

--- public_html/view/editFormation.php
+++ public_html/view/editFormation.php
@@ -1,5 +1,6 @@
 <!-- a changer -->
 <link href="view/css/admin.css" rel="stylesheet" type="text/css">
+<link href="view/css/editFormation.css" rel="stylesheet" type="text/css">
 
 <?php
 $stmt=getUserFormation($_SESSION['id'], $_GET['id'])->fetch();
@@ -7,20 +8,25 @@ $stmt=getUserFormation($_SESSION['id'], $_GET['id'])->fetch();
 
 
 <form class="" action="<?php echo("index.php?action=editFormation&id=".$_GET['id']); ?>" method="post">
-  <label for="name">Nom</label>
-  <input type="text" name="name" value="<?php echo(htmlspecialchars($stmt['name'])); ?>"><br/>
-  <label for="duration">Durée en heures</label>
-  <input type="number" name="duration" value="<?php echo(htmlspecialchars($stmt['duration'])); ?>"><br/>
-  <label for="content">formation content (MarkDown)</label>
-  <textarea id="story" name="content"
-            rows="20" cols="50"><?php echo(htmlspecialchars($stmt['content'])); ?></textarea><br>
-  <button type="submit" name="button">commit</button>
+  <section id="properties">
+    <label for="name">Nom</label>
+    <input type="text" name="name" value="<?php echo(htmlspecialchars($stmt['name'])); ?>">
+    <label for="duration">Durée en heures</label>
+    <input type="number" name="duration" value="<?php echo(htmlspecialchars($stmt['duration'])); ?>">
+  </section>
+  <section id="contentFormation">
+    <label for="content">formation content (MarkDown)</label>
+    <textarea id="story" name="content"
+              rows="20" cols="50"><?php echo(htmlspecialchars($stmt['content'])); ?></textarea>
+    <button type="submit" name="button">commit</button>
+  </section>
 </form>
 
 <br/>
 <br/>
 
-<h2>Preview</h2>
+<h1>Preview</h1>
 <br/>
-
-<?php echo(buildMarkdown($stmt['content'])) ?>
+<section id="preview">
+  <?php echo(buildMarkdown($stmt['content'])) ?>
+</section>
