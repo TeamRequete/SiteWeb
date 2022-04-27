2a09074b5c93e9d178ce7481a0a12b3de9d0d90e
requin-citron <requin_citron@protonmail.com>
2022-04-27 13:52:15 +0200

    disable corr qcm

--- public_html/view/qcm_cor.php
+++ public_html/view/qcm_cor.php
@@ -13,13 +13,13 @@ $xml = simplexml_load_string($stmt['qcm']);
     <legend> <?php echo(htmlentities($curr->problem[0])); ?> </legend><br>
     <?php for($d=0;$d<(count($curr->proposes->proposetrue));$d++): ?>
       <div>
-          <input type="checkbox" id="<?php echo($d); ?>" name="<?php echo(strval($i)."|".strval($d)); ?>" value="<?php echo(htmlentities($curr->proposes->proposetrue[$d])) ?>" checked>
+          <input type="checkbox" id="<?php echo($d); ?>" name="<?php echo(strval($i)."|".strval($d)); ?>" value="<?php echo(htmlentities($curr->proposes->proposetrue[$d])) ?>" checked disabled>
           <label for="<?php echo($d); ?>"><?php echo(htmlentities($curr->proposes->proposetrue[$d])); ?></label>
       </div>
     <?php endfor; ?>
     <?php for($d=0;$d<(count($curr->proposes->propose));$d++): ?>
       <div>
-          <input type="checkbox" id="<?php echo($d); ?>" name="<?php echo(strval($i)."|".strval($d)); ?>" value="<?php echo(htmlentities($curr->proposes->propose[$d])) ?>">
+          <input type="checkbox" id="<?php echo($d); ?>" name="<?php echo(strval($i)."|".strval($d)); ?>" value="<?php echo(htmlentities($curr->proposes->propose[$d])) ?>" disabled>
           <label for="<?php echo($d); ?>"><?php echo(htmlentities($curr->proposes->propose[$d])); ?></label>
       </div>
     <?php endfor; ?>
